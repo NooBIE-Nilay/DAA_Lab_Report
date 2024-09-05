836a71f5dce59f35bb8ee4a8c8a9852984181172
Nilay Banerjee <n00bie@Nilays-MacBook-Air.local>
2024-09-05 13:30:11 +0530

    Refactored Code According to Class Standards

--- 8123_6_1_Prims.c
+++ 8123_6_1_Prims.c
@@ -10,184 +10,184 @@ typedef struct
 {
     int vertex;
     int weight;
-} Edge;
+} Edge_123;
 
 typedef struct
 {
-    Edge *array;
+    Edge_123 *array;
     int size;
     int capacity;
-} MinHeap;
+} MinHeap_123;
 
-MinHeap *createMinHeap(int capacity)
+MinHeap_123 *createMinHeap_123(int capacity)
 {
-    MinHeap *minHeap = (MinHeap *)malloc(sizeof(MinHeap));
-    minHeap->size = 0;
-    minHeap->capacity = capacity;
-    minHeap->array = (Edge *)malloc(capacity * sizeof(Edge));
-    return minHeap;
+    MinHeap_123 *minHeap_123 = (MinHeap_123 *)malloc(sizeof(MinHeap_123));
+    minHeap_123->size = 0;
+    minHeap_123->capacity = capacity;
+    minHeap_123->array = (Edge_123 *)malloc(capacity * sizeof(Edge_123));
+    return minHeap_123;
 }
 
-void swapEdge(Edge *a, Edge *b)
+void swapEdge_123(Edge_123 *a_123, Edge_123 *b_123)
 {
-    Edge t = *a;
-    *a = *b;
-    *b = t;
+    Edge_123 t_123 = *a_123;
+    *a_123 = *b_123;
+    *b_123 = t_123;
 }
 
-void minHeapify(MinHeap *minHeap, int idx)
+void minHeapify_123(MinHeap_123 *minHeap_123, int idx_123)
 {
-    int smallest = idx;
-    int left = 2 * idx + 1;
-    int right = 2 * idx + 2;
+    int smallest_123 = idx_123;
+    int left_123 = 2 * idx_123 + 1;
+    int right_123 = 2 * idx_123 + 2;
 
-    if (left < minHeap->size && minHeap->array[left].weight < minHeap->array[smallest].weight)
-        smallest = left;
+    if (left_123 < minHeap_123->size && minHeap_123->array[left_123].weight < minHeap_123->array[smallest_123].weight)
+        smallest_123 = left_123;
 
-    if (right < minHeap->size && minHeap->array[right].weight < minHeap->array[smallest].weight)
-        smallest = right;
+    if (right_123 < minHeap_123->size && minHeap_123->array[right_123].weight < minHeap_123->array[smallest_123].weight)
+        smallest_123 = right_123;
 
-    if (smallest != idx)
+    if (smallest_123 != idx_123)
     {
-        swapEdge(&minHeap->array[smallest], &minHeap->array[idx]);
-        minHeapify(minHeap, smallest);
+        swapEdge_123(&minHeap_123->array[smallest_123], &minHeap_123->array[idx_123]);
+        minHeapify_123(minHeap_123, smallest_123);
     }
 }
 
-Edge extractMin(MinHeap *minHeap)
+Edge_123 extractMin_123(MinHeap_123 *minHeap_123)
 {
-    Edge root = minHeap->array[0];
-    minHeap->array[0] = minHeap->array[minHeap->size - 1];
-    --minHeap->size;
-    minHeapify(minHeap, 0);
-    return root;
+    Edge_123 root_123 = minHeap_123->array[0];
+    minHeap_123->array[0] = minHeap_123->array[minHeap_123->size - 1];
+    --minHeap_123->size;
+    minHeapify_123(minHeap_123, 0);
+    return root_123;
 }
 
-void insertMinHeap(MinHeap *minHeap, Edge edge)
+void insertMinHeap_123(MinHeap_123 *minHeap_123, Edge_123 edge_123)
 {
-    ++minHeap->size;
-    int i = minHeap->size - 1;
-    while (i && edge.weight < minHeap->array[(i - 1) / 2].weight)
+    ++minHeap_123->size;
+    int i = minHeap_123->size - 1;
+    while (i && edge_123.weight < minHeap_123->array[(i - 1) / 2].weight)
     {
-        minHeap->array[i] = minHeap->array[(i - 1) / 2];
+        minHeap_123->array[i] = minHeap_123->array[(i - 1) / 2];
         i = (i - 1) / 2;
     }
-    minHeap->array[i] = edge;
+    minHeap_123->array[i] = edge_123;
 }
 
-void primMST(int **graph, int n, int start, FILE *file)
+void primMST_123(int **graph_123, int n_123, int start_123, FILE *file_123)
 {
-    int parent[n];
-    int key[n];
-    int inMST[n];
-    MinHeap *minHeap = createMinHeap(n);
+    int parent_123[n_123];
+    int key_123[n_123];
+    int inMST_123[n_123];
+    MinHeap_123 *minHeap_123 = createMinHeap_123(n_123);
 
-    for (int v = 0; v < n; ++v)
+    for (int v = 0; v < n_123; ++v)
     {
-        key[v] = INT32_MAX;
-        inMST[v] = 0;
-        minHeap->array[v].vertex = v;
-        minHeap->array[v].weight = key[v];
+        key_123[v] = INT32_MAX;
+        inMST_123[v] = 0;
+        minHeap_123->array[v].vertex = v;
+        minHeap_123->array[v].weight = key_123[v];
     }
 
-    minHeap->array[start].vertex = start;
-    minHeap->array[start].weight = key[start] = 0;
-    minHeap->size = n;
+    minHeap_123->array[start_123].vertex = start_123;
+    minHeap_123->array[start_123].weight = key_123[start_123] = 0;
+    minHeap_123->size = n_123;
 
-    while (minHeap->size)
+    while (minHeap_123->size)
     {
-        Edge minEdge = extractMin(minHeap);
-        int u = minEdge.vertex;
-        inMST[u] = 1;
+        Edge_123 minEdge_123 = extractMin_123(minHeap_123);
+        int u = minEdge_123.vertex;
+        inMST_123[u] = 1;
 
-        for (int v = 0; v < n; ++v)
+        for (int v = 0; v < n_123; ++v)
         {
-            if (graph[u][v] && inMST[v] == 0 && graph[u][v] < key[v])
+            if (graph_123[u][v] && inMST_123[v] == 0 && graph_123[u][v] < key_123[v])
             {
-                key[v] = graph[u][v];
-                parent[v] = u;
-                insertMinHeap(minHeap, (Edge){v, key[v]});
+                key_123[v] = graph_123[u][v];
+                parent_123[v] = u;
+                insertMinHeap_123(minHeap_123, (Edge_123){v, key_123[v]});
             }
         }
     }
 
-    int totalCost = 0;
-    int **mst = (int **)malloc(n * sizeof(int *));
-    for (int i = 0; i < n; i++)
+    int totalCost_123 = 0;
+    int **mst_123 = (int **)malloc(n_123 * sizeof(int *));
+    for (int i = 0; i < n_123; i++)
     {
-        mst[i] = (int *)malloc(n * sizeof(int));
-        for (int j = 0; j < n; j++)
+        mst_123[i] = (int *)malloc(n_123 * sizeof(int));
+        for (int j = 0; j < n_123; j++)
         {
-            mst[i][j] = 0;
+            mst_123[i][j] = 0;
         }
     }
 
-    for (int i = 1; i < n; ++i)
+    for (int i = 1; i < n_123; ++i)
     {
-        mst[parent[i]][i] = graph[parent[i]][i];
-        mst[i][parent[i]] = graph[parent[i]][i];
-        totalCost += graph[parent[i]][i];
+        mst_123[parent_123[i]][i] = graph_123[parent_123[i]][i];
+        mst_123[i][parent_123[i]] = graph_123[parent_123[i]][i];
+        totalCost_123 += graph_123[parent_123[i]][i];
     }
 
     printf("Cost adjacency matrix of the minimum spanning tree:\n");
-    for (int i = 0; i < n; ++i)
+    for (int i = 0; i < n_123; ++i)
     {
-        for (int j = 0; j < n; ++j)
+        for (int j = 0; j < n_123; ++j)
         {
-            printf("%d ", mst[i][j]);
-            fprintf(file, "%d ", mst[i][j]);
+            printf("%d ", mst_123[i][j]);
+            fprintf(file_123, "%d ", mst_123[i][j]);
         }
         printf("\n");
-        fprintf(file, "\n");
+        fprintf(file_123, "\n");
     }
-    printf("Total cost of the minimum spanning tree: %d\n", totalCost);
-    fprintf(file, "Total cost of the minimum spanning tree: %d\n", totalCost);
-    fclose(file);
+    printf("Total cost of the minimum spanning tree: %d\n", totalCost_123);
+    fprintf(file_123, "Total cost of the minimum spanning tree: %d\n", totalCost_123);
+    fclose(file_123);
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n_123; i++)
     {
-        free(mst[i]);
+        free(mst_123[i]);
     }
-    free(mst);
-    free(minHeap->array);
-    free(minHeap);
+    free(mst_123);
+    free(minHeap_123->array);
+    free(minHeap_123);
 }
 
 int main()
 {
-    FILE *file = fopen("inputs/8123_6_1_inUnAdjMat.dat", "r");
-    if (!file)
+    FILE *file_123 = fopen("inputs/8123_6_1_inUnAdjMat.dat", "r");
+    if (!file_123)
     {
-        printf("Unable to open file.\n");
+        printf("Unable to open file_123.\n");
         return 1;
     }
-    int n;
+    int n_123;
     printf("Enter the Number Of Vertices: ");
-    scanf("%d", &n);
+    scanf("%d", &n_123);
 
-    int **graph = (int **)malloc(n * sizeof(int *));
-    for (int i = 0; i < n; i++)
+    int **graph_123 = (int **)malloc(n_123 * sizeof(int *));
+    for (int i = 0; i < n_123; i++)
     {
-        graph[i] = (int *)malloc(n * sizeof(int));
-        for (int j = 0; j < n; j++)
+        graph_123[i] = (int *)malloc(n_123 * sizeof(int));
+        for (int j = 0; j < n_123; j++)
         {
-            fscanf(file, "%d", &graph[i][j]);
-            if (graph[i][j] == 0)
-                graph[i][j] = INT32_MAX;
+            fscanf(file_123, "%d", &graph_123[i][j]);
+            if (graph_123[i][j] == 0)
+                graph_123[i][j] = INT32_MAX;
         }
     }
 
-    fclose(file);
-    file = fopen("outputs/8123_6_1_outUnAdjMat.dat", "w");
-    int start;
+    fclose(file_123);
+    file_123 = fopen("outputs/8123_6_1_outUnAdjMat.dat", "w");
+    int start_123;
     printf("Enter the starting vertex: ");
-    scanf("%d", &start);
-    start--;
-    primMST(graph, n, start, file);
-    for (int i = 0; i < n; i++)
+    scanf("%d", &start_123);
+    start_123--;
+    primMST_123(graph_123, n_123, start_123, file_123);
+    for (int i = 0; i < n_123; i++)
     {
-        free(graph[i]);
+        free(graph_123[i]);
     }
-    free(graph);
+    free(graph_123);
     return 0;
 }
